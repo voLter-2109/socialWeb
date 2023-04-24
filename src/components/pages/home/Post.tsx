fef10af617ba83d8feadbcb9926d69c7f9ab147a
Sausan Felchina <youen@mail.ru>
2023-04-24 23:05:07 +0300

    1

--- src/components/pages/home/Post.tsx
+++ src/components/pages/home/Post.tsx
@@ -24,6 +24,7 @@ const Post: React.FC = () => {
     const unsub = new Promise((resolve, reject) => {
       onSnapshot(collection(db, "post"), (doc: any) => {
         resolve(doc);
+        console.log("3")
       });
     });
 
